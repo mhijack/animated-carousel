c081e90a4ad09e39afae569393d48db01f83c1a2
Jianyuan Chen <jianyuan.chen12@gmail.com>
2018-11-03 17:54:48 -0700

    Updates

---
+++ precache-manifest.8ee8fee6de871817dfb7a004e6725ded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c0067298d69be2fd1de",
+    "url": "/animated-carousel/static/js/runtime~main.7c006729.js"
+  },
+  {
+    "revision": "7472ced1a08473aed424",
+    "url": "/animated-carousel/static/js/main.7472ced1.chunk.js"
+  },
+  {
+    "revision": "366e4aa335eaa55a86d7",
+    "url": "/animated-carousel/static/js/1.366e4aa3.chunk.js"
+  },
+  {
+    "revision": "7472ced1a08473aed424",
+    "url": "/animated-carousel/static/css/main.108ebbe0.chunk.css"
+  },
+  {
+    "revision": "e49baed3abd641ad6ee0c7def496ff8e",
+    "url": "/animated-carousel/index.html"
+  }
+];
